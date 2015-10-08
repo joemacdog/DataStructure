726f289f4c7dad8993ac7ce4fb28ad1f831433f8
wowiwj <35649084@qq.com>
2015-10-08 23:34:49 +0800

    初始化

---
+++ 01-三种方法的实现/01-三种方法的实现/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  01-三种方法的实现
+//
+//  Created by wangju on 15/10/8.
+//  Copyright © 2015年 wangju. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
